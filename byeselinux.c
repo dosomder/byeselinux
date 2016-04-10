987940678e7c83090c003affda819d42562db5cd
dosomder <dosomder@users.noreply.github.com>
2016-04-10 23:05:23 +0200

    Add 64bit support

--- byeselinux.c
+++ byeselinux.c
@@ -7,7 +7,7 @@
 
 MODULE_AUTHOR("zxz0O0");
 MODULE_DESCRIPTION("Say bye to SELinux at boot time to allow dualrecovery");
-MODULE_VERSION("1.1");
+MODULE_VERSION("1.2");
 MODULE_LICENSE("GPL");
 
 #ifdef REPLACE_MODULE
@@ -27,9 +27,41 @@ unsigned int enforcing;
 unsigned int* _selinux_enabled = NULL;
 unsigned int* _selinux_enforcing = NULL;
 
+#if __LP64__
+void* parseadrp(unsigned char* pc, int cmd)
+{
+	long offset;
+	void* addr;
+	//ADRP X??, #0x????
+	int cmdid = (cmd >> 24) & 0xFF;
+	if(cmdid != 0xB0 && cmdid != 0x90)
+		return NULL;
+	//int register = (cmd & 0xF) + (cmd & 0x10);
+	offset = (((cmd >> 4) & 0xFFFFF) & ~1) * 2 * 4096;
+	addr = (void*)((unsigned long)(pc + offset) & 0xFFFFFFFFFFFFF000);
+	if(cmdid == 0xB0)
+		addr = (unsigned char*)addr + 4096;
+
+	return addr;
+}
+
+int parseldr(int cmd)
+{
+	int offset;
+	int cmdid =  (cmd >> 20) & 0xFFF;
+	//LDR W??, [X??, #0x???]
+	if(cmdid != 0xB94)
+		return -1;
+	//int toregister = (cmd & 0xF) + (cmd & 0x10);
+	//int fromregister = (((cmd >> 4) & 0xE) >> 1) + (((cmd >> 8) & 0x3) << 3)
+	offset = (cmd >> 8) & 0xFFF;
+	return offset;
+}
+#endif
+
 unsigned int* findEnabled(void)
 {
-#if !defined(__LP64__) || !__LP64__
+#if !__LP64__
 	int i = 0;
 	//0x1e,0xff,0x2f,0xe1 --> bx lr
 	const char asm_bx[] = { 0x1E, 0xFF, 0x2F, 0xE1 };
@@ -39,27 +71,58 @@ unsigned int* findEnabled(void)
 		if(memcmp(&p[i], asm_bx, 4) == 0)
 			return *(unsigned int**)&p[i+4];
 	}
-#endif
 
 	return NULL;
+#else
+	unsigned char* page;
+	int pageoff;
+	char* p = (char*)kallsyms_lookup_name("selinux_is_enabled");
+	if(!p)
+		return NULL;
+
+	if(!(page = parseadrp(p, *(int*)p)))
+		return NULL;
+	pageoff = parseldr(*((int*)p + 1));
+	if(pageoff == -1)
+		return NULL;
+
+	return (unsigned int*)(page + pageoff);
+#endif
 }
 
 unsigned int* findEnforcing(void)
 {
-#if !defined(__LP64__) || !__LP64__
+#if !__LP64__
 	int i = 0;
 	//0xf0,0x80,0xbd,0xe8 --> LDMFD SP!, {R4-R7,PC}
 	const char asm_ldmfd[] = { 0xF0, 0x80, 0xBD, 0xE8 };
 	char* p = (char*)kallsyms_lookup_name("sel_read_enforce");
 	if(p == NULL)
-		return 0;
+		return NULL;
 	for(i = 0; i < 128; i++)
 	{
 		if(memcmp(&p[i], asm_ldmfd, 4) == 0)
 			return *(unsigned int**)&p[i+12];
 	}
-#endif
+#else
+	int i = 0;
+	unsigned char* page = NULL;
+	char* p = (char*)kallsyms_lookup_name("sel_read_enforce");
+	if(!p)
+		return NULL;
 
+	for(i = 0; i < 92; i += 4, p += 4)
+	{
+		if(!page)
+			page = parseadrp(p, *(int*)p);
+		else
+		{
+			int pageoff = parseldr(*(int*)p);
+			if(pageoff != -1)
+				return (unsigned int*)(page + pageoff);
+		}
+	}
+#endif
 	return NULL;
 }
 
